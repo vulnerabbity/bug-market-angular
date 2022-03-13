c24c4e81e3cbfe79bb6ca9bc7e2a71ce26146bbc
vulnerabbity <you@example.com>
2022-03-13 23:01:06 +0700

    bugfix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
-import { AppComponent } from "./app.component"
+import { AppComponent } from "./components/app/app.component"
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import { MaterialModule } from "./modules/material.module"
 import { GraphqlModule } from "./modules/graphql.module"
