508c7e4db499e2d7fd92fdbe59e4c9096f81e46d
vulnerabbity <you@example.com>
2022-03-15 14:11:13 +0700

    add header

--- src/app/pages/products/products-page.module.ts
+++ src/app/pages/products/products-page.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from "@angular/core"
+import { AppCommonComponentsModule } from "src/app/common/modules/components.module"
 import { ProductsPageComponent } from "./products-page.component"
 
 @NgModule({
+  imports: [AppCommonComponentsModule],
   declarations: [ProductsPageComponent]
 })
 export class ProductsPageModule {}
