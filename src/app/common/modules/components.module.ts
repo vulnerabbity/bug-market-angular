0d16bdb8cd2bb7f535434f22089a368c61a30135
vulnerabbity <you@example.com>
2022-03-16 13:36:16 +0700

    add product card and div

--- src/app/common/modules/components.module.ts
+++ src/app/common/modules/components.module.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { MaterialModule } from "../../modules/material.module"
 import { CommonPageHeaderComponent } from "../components/page-header/page-header.component"
+import { CommonProductCardComponent } from "../components/product-card/product-card.component"
+import { CommonThemedDiv } from "../components/themed-div/themed-div.component"
 
-const passThroughModules = [MaterialModule]
-const passThroughComponents = [CommonPageHeaderComponent]
+const passThroughModules = [MaterialModule, CommonModule]
+const passThroughComponents = [
+  CommonPageHeaderComponent,
+  CommonProductCardComponent,
+  CommonThemedDiv
+]
 
 @NgModule({
   imports: passThroughModules,
