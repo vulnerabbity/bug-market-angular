9419b2a9e73d429d597f6fdf4154697c6561343d
vulnerabbity <you@example.com>
2022-03-16 13:35:12 +0700

    add card

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -3,8 +3,15 @@ import { MatButtonModule } from "@angular/material/button"
 import { MatToolbarModule } from "@angular/material/toolbar"
 import { MatIconModule } from "@angular/material/icon"
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"
+import { MatCardModule } from "@angular/material/card"
 
-const materialModules = [MatButtonModule, MatToolbarModule, MatIconModule, MatProgressSpinnerModule]
+const materialModules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatCardModule
+]
 
 @NgModule({
   imports: materialModules,
