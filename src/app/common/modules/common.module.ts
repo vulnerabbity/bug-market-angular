59ae385c26b81755758fa5404db37224fceed962
vulnerabbity <you@example.com>
2022-03-16 13:35:32 +0700

    add angular common module

--- src/app/common/modules/common.module.ts
+++ src/app/common/modules/common.module.ts
@@ -1,7 +1,8 @@
+import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { AppCommonComponentsModule } from "./components.module"
 
-const passThroughModules = [AppCommonComponentsModule]
+const passThroughModules = [CommonModule, AppCommonComponentsModule]
 
 @NgModule({
   imports: passThroughModules,
