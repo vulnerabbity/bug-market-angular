292d230844dd4ac031f31140b72d8065285014a3
vulnerabbity <you@example.com>
2022-03-19 12:07:54 +0700

    add animations

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -5,10 +5,19 @@ import { GraphqlModule } from "../modules/graphql.module"
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { AppNavbarComponent } from "./navbar/navbar.component"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+
+const imports = [
+  BrowserModule,
+  AppRoutingModule,
+  AppCommonModule,
+  BrowserAnimationsModule,
+  GraphqlModule
+]
 
 @NgModule({
   declarations: [AppComponent, AppNavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, AppCommonModule, GraphqlModule],
+  imports,
   providers: [],
   bootstrap: [AppComponent]
 })
