7d4ccab5d0a214ada0c883194d2ac19c31ea1c78
vulnerabbity <you@example.com>
2022-03-19 23:06:56 +0700

    add full product

--- src/app/features/products/products.service.ts
+++ src/app/features/products/products.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from "@angular/core"
-import { ShortProductsGQL, ShortProductsQueryVariables } from "src/generated-gql-types"
+import {
+  FullProductGQL,
+  Product,
+  ShortProductsGQL,
+  ShortProductsQueryVariables
+} from "src/generated-gql-types"
 import { Observable, pluck, take } from "rxjs"
 import { PaginatedShortProduct } from "./products.interface"
 
@@ -7,10 +12,19 @@ import { PaginatedShortProduct } from "./products.interface"
   providedIn: "root"
 })
 export class ProductsService {
-  constructor(private shortProductsQuery: ShortProductsGQL) {}
+  constructor(
+    private shortProductsQuery: ShortProductsGQL,
+    private fullProductQuery: FullProductGQL
+  ) {}
   loadShortProducts(options?: ShortProductsQueryVariables): Observable<PaginatedShortProduct> {
     const response = this.shortProductsQuery.fetch(options)
-    const parsedResponse = response.pipe(pluck("data", "products"), take(1))
+    const parsedResponse = response.pipe(pluck("data", "products"))
+    return parsedResponse
+  }
+
+  loadFullProduct(id: string): Observable<Product> {
+    const response = this.fullProductQuery.fetch({ id })
+    const parsedResponse = response.pipe(pluck("data", "product"))
     return parsedResponse
   }
 }
