76689e9d5b8870301a1e2dd55f157e7f29475bfe
vulnerabbity <you@example.com>
2022-03-19 12:17:49 +0700

    add cards components

--- src/app/common/modules/components.module.ts
+++ src/app/common/modules/components.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from "@angular/core"
 import { MaterialModule } from "../../modules/material.module"
 import { CommonPageHeaderComponent } from "../components/page-header/page-header.component"
 import { CommonProductCardComponent } from "../components/product-card/product-card.component"
+import { CommonProductsContainer } from "../components/products-container/products-container.component"
 import { CommonThemedDiv } from "../components/themed-div/themed-div.component"
 
 const passThroughModules = [MaterialModule, CommonModule]
 const passThroughComponents = [
   CommonPageHeaderComponent,
   CommonProductCardComponent,
-  CommonThemedDiv
+  CommonThemedDiv,
+  CommonProductCardComponent,
+  CommonProductsContainer
 ]
 
 @NgModule({
