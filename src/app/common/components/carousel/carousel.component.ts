84c1ce050e71da3f6b596c0f3eee6444ac1e2e7b
vulnerabbity <you@example.com>
2022-03-21 13:10:18 +0700

    add valid initialization

--- src/app/common/components/carousel/carousel.component.ts
+++ src/app/common/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core"
+import { AfterViewInit, Component, Input, OnInit } from "@angular/core"
 import { OwlOptions } from "ngx-owl-carousel-o"
 
 @Component({
@@ -6,20 +6,9 @@ import { OwlOptions } from "ngx-owl-carousel-o"
   templateUrl: "./carousel.component.html",
   styleUrls: ["./carousel.component.scss"]
 })
-export class CommonCarousel implements OnInit {
+export class CommonCarousel implements AfterViewInit {
   @Input()
-  images: string[] = [
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.toKUHaOWwBaYjmcQ9Cp5UQHaJP%26pid%3DApi&f=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.pitchfork.com%2Fphotos%2F5f3b196deb982d18c453e0f1%2F2%3A1%2Fw_2560%252Cc_limit%2Fbarack-obama.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1",
-    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.ruposters.ru%2Fnewslead%2F8%2F80e6e8096fc875139708b7a5689d3785.jpg&f=1&nofb=1"
-  ]
+  images: string[] = []
 
   customOptions: OwlOptions = {
     loop: false,
@@ -37,17 +26,22 @@ export class CommonCarousel implements OnInit {
     nav: true
   }
 
-  ngOnInit(): void {
+  // after slider loaded
+  ngAfterViewInit(): void {
     this.initThumbnails()
+    this.initNavigation()
   }
-  // dot.innerHTML = `<img src="${this.images[0]}"/>`
 
-  initThumbnails() {
+  private initThumbnails() {
     const dots = document.getElementsByClassName("owl-dot")
-    setTimeout(() => {
-      for (let i = 0; i < dots.length; i++) {
-        dots[i].innerHTML = `<img src="${this.images[i]}"/>`
-      }
-    }, 1000)
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].innerHTML = `<img src="${this.images[i]}"/>`
+    }
+  }
+
+  private initNavigation() {
+    const navigation = document.getElementsByClassName("owl-nav")[0]
+    const navigationDestination = document.getElementsByClassName("owl-stage-outer")[0]
+    navigationDestination.appendChild(navigation)
   }
 }
