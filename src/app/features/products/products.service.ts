981a1d40eeca235a7526879e74572a1ebd4b17ee
vulnerabbity <you@example.com>
2022-03-21 16:06:35 +0700

    add image path converting

--- src/app/features/products/products.service.ts
+++ src/app/features/products/products.service.ts
@@ -5,8 +5,9 @@ import {
   ShortProductsGQL,
   ShortProductsQueryVariables
 } from "src/generated-gql-types"
-import { Observable, pluck, take } from "rxjs"
-import { PaginatedShortProduct } from "./products.interface"
+import { map, Observable, pluck } from "rxjs"
+import { PaginatedShortProducts } from "./products.interface"
+import { FilesService } from "src/app/common/services/files.service"
 
 @Injectable({
   providedIn: "root"
@@ -14,17 +15,36 @@ import { PaginatedShortProduct } from "./products.interface"
 export class ProductsService {
   constructor(
     private shortProductsQuery: ShortProductsGQL,
-    private fullProductQuery: FullProductGQL
+    private fullProductQuery: FullProductGQL,
+    private filesService: FilesService
   ) {}
-  loadShortProducts(options?: ShortProductsQueryVariables): Observable<PaginatedShortProduct> {
+  loadShortProducts(options?: ShortProductsQueryVariables): Observable<PaginatedShortProducts> {
     const response = this.shortProductsQuery.fetch(options)
     const parsedResponse = response.pipe(pluck("data", "products"))
-    return parsedResponse
+    const responseWithPaths = parsedResponse.pipe(
+      map(paginatedProducts => {
+        paginatedProducts = { ...paginatedProducts }
+        paginatedProducts.data = paginatedProducts.data.map(product => this.addImagePath(product))
+        return paginatedProducts
+      })
+    )
+
+    return responseWithPaths
   }
 
   loadFullProduct(id: string): Observable<Product> {
     const response = this.fullProductQuery.fetch({ id })
-    const parsedResponse = response.pipe(pluck("data", "product"))
+    let parsedResponse = response.pipe(
+      pluck("data", "product"),
+      map(product => this.addImagePath(product))
+    )
+
     return parsedResponse
+  }
+
+  private addImagePath<T extends { imagesUrls: string[] }>(product: T) {
+    product = { ...product }
+    product.imagesUrls = this.filesService.addPathToPublicFiles(product.imagesUrls)
+    return product
   }
 }
