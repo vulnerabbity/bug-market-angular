e46167a595174ec5e8b1322c1a92b042e72709e3
vulnerabbity <you@example.com>
2022-03-22 15:49:16 +0700

    initial

---
+++ src/assets/assets.paths.ts
@@ -0,0 +1,7 @@
+const picturesPath = "assets/pictures"
+
+export const assetsPaths = {
+  Pictures: picturesPath,
+  NoAvatar: `${picturesPath}/no-avatar.png`,
+  NoImage: `${picturesPath}/no-image.webp`
+}
