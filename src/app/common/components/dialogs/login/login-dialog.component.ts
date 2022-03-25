4f2da8151824457f1db2bb7bf6ff3c260e2672b2
vulnerabbity <you@example.com>
2022-03-25 18:38:49 +0700

    add requests display

--- src/app/common/components/dialogs/login/login-dialog.component.ts
+++ src/app/common/components/dialogs/login/login-dialog.component.ts
@@ -2,16 +2,29 @@ import { Component, OnInit } from "@angular/core"
 import { MatDialog, MatDialogRef } from "@angular/material/dialog"
 import { CommonRegisterDialogComponent } from "../register/register-dialog.component"
 import { FormFieldModel } from "../../form-fields/components/abstract-form-field"
+import { AuthenticationService } from "src/app/features/authentication/authentication.service"
 
 @Component({
   templateUrl: "./login-dialog.component.html",
   styleUrls: ["../login-register.shared.scss", "./login-dialog.component.scss"]
 })
 export class CommonLoginDialogComponent implements OnInit {
+  loading = false
+  requestError = ""
+
   usernameField: FormFieldModel = { value: "", isValid: false }
   passwordField: FormFieldModel = { value: "", isValid: false }
 
+  get username(): string {
+    return this.usernameField.value
+  }
+
+  get password(): string {
+    return this.passwordField.value
+  }
+
   constructor(
+    private authentication: AuthenticationService,
     private dialog: MatDialog,
     private currentDialog: MatDialogRef<CommonLoginDialogComponent>
   ) {}
@@ -27,8 +40,31 @@ export class CommonLoginDialogComponent implements OnInit {
     }, 100)
   }
 
-  onLogin() {
-    console.log(this.usernameField)
+  async onLogin(): Promise<void> {
+    this.loading = true
+
+    const status = await this.authentication.loginWithUsername({
+      username: this.username,
+      password: this.password
+    })
+
+    if (status === "success") {
+      this.currentDialog.close()
+    }
+
+    if (status === "not-found") {
+      this.requestError = "User does not exists"
+    }
+
+    if (status === "invalid-password") {
+      this.requestError = "Invalid password"
+    }
+
+    if (status === "misc") {
+      this.requestError = "Unknown error"
+    }
+
+    this.loading = false
   }
 
   ngOnInit(): void {
