e558a17d02d831b8f5426556c5b18607b56b228f
vulnerabbity <you@example.com>
2022-03-26 20:56:49 +0700

    add navbar with create product link

--- src/app/pages/products/products-page.module.ts
+++ src/app/pages/products/products-page.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core"
 import { AppCommonModule } from "src/app/common/modules/common.module"
 import { ProductsModule } from "src/app/features/products/products.module"
+import { ProductsNavbarComponent } from "./navbar/products-navbar.component"
 import { ProductsPageComponent } from "./products-page.component"
 
 @NgModule({
   imports: [AppCommonModule, ProductsModule],
-  declarations: [ProductsPageComponent]
+  declarations: [ProductsPageComponent, ProductsNavbarComponent]
 })
 export class ProductsPageModule {}
