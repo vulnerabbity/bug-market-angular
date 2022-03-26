e4532a66070d23069127753546b319a5ed8a8ebe
vulnerabbity <you@example.com>
2022-03-26 22:43:24 +0700

    add access and refresh tokens methods

--- src/app/features/local-storage/tokens.service.ts
+++ src/app/features/local-storage/tokens.service.ts
@@ -1,7 +1,24 @@
 import { AccessTokenLocalStorageService } from "./access-token.service"
 import { RefreshTokenLocalStorageService } from "./refresh-token.service"
 
+interface AccessAndRefreshToken {
+  accessToken: string
+  refreshToken: string
+}
+
 export class TokensLocalStorageService {
   access = new AccessTokenLocalStorageService()
   refresh = new RefreshTokenLocalStorageService()
+
+  tryToGetAccessAndRefreshTokens(): AccessAndRefreshToken {
+    const accessToken = this.access.tryToGetRecord()
+    const refreshToken = this.refresh.tryToGetRecord()
+    return { accessToken, refreshToken }
+  }
+
+  isAccessAndRefreshTokenExists(): boolean {
+    const isAccessTokenExist = this.access.isRecordExists()
+    const isRefreshTokenExists = this.refresh.isRecordExists()
+    return isAccessTokenExist && isRefreshTokenExists
+  }
 }
