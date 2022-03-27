e49bba12f32abd65b33283221a219b0cc830b998
vulnerabbity <you@example.com>
2022-03-27 19:54:32 +0700

    add fuse.js fuzzysearch

--- src/app/features/categories/categories.service.ts
+++ src/app/features/categories/categories.service.ts
@@ -1,8 +1,17 @@
 import { ProductCategory } from "./categories.interface"
 import { CategoriesList } from "./categories.data"
+import { Injectable } from "@angular/core"
+import Fuse from "fuse.js"
 
+const fuseConfig: Fuse.IFuseOptions<ProductCategory> = {
+  threshold: 0.6,
+  keys: ["visualName", "databaseName"]
+}
+
+@Injectable({ providedIn: "root" })
 export class ProductCategoriesService {
-  localCategories = CategoriesList
+  private readonly localCategories = CategoriesList
+  private fuseSearcher: Fuse<ProductCategory> = new Fuse(this.getCategoriesLocal(), fuseConfig)
 
   /**
    * Gets categories list from client.
@@ -10,5 +19,22 @@ export class ProductCategoriesService {
    */
   getCategoriesLocal(): ProductCategory[] {
     return this.localCategories
+  }
+
+  fuzzySearchLocal(search: string): ProductCategory[] {
+    if (search === "") {
+      return this.getCategoriesLocal()
+    }
+    return this.searchWitFuse(search)
+  }
+
+  private searchWitFuse(search: string): ProductCategory[] {
+    const fuseResult = this.fuseSearcher.search(search)
+    const paredResult = this.parseFuseResult(fuseResult)
+    return paredResult
+  }
+
+  private parseFuseResult<T>(fuseResult: Fuse.FuseResult<T>[]): T[] {
+    return fuseResult.map(result => result.item)
   }
 }
