1950dea9292ea21d1414b312c317fae2567fe020
vulnerabbity <you@example.com>
2022-03-27 19:52:29 +0700

    add price field

--- src/app/common/components/form-fields/form-fields.declarations.ts
+++ src/app/common/components/form-fields/form-fields.declarations.ts
@@ -1,9 +1,11 @@
 import { CommonProductNameFieldComponent } from "./components/product-name.component"
 import { CommonPasswordFieldComponent } from "./components/password.component"
 import { CommonUsernameFieldComponent } from "./components/username.component"
+import { CommonProductPriceFieldComponent } from "./components/product-price.component"
 
 export const commonAppFormFieldsComponents = [
   CommonPasswordFieldComponent,
   CommonUsernameFieldComponent,
-  CommonProductNameFieldComponent
+  CommonProductNameFieldComponent,
+  CommonProductPriceFieldComponent
 ]
