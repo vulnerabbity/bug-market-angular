440861b6d24fcd64dae911d1beabf71882328be6
vulnerabbity <you@example.com>
2022-03-27 19:53:21 +0700

    add min max

--- src/app/common/forms/forms.error-detector.ts
+++ src/app/common/forms/forms.error-detector.ts
@@ -8,16 +8,25 @@ export class FormsErrorDetector {
   }
 
   isRequiredError(): boolean {
-    if (!this.formControl.errors) {
-      return false
-    }
-    return !!this.formControl.errors["required"]
+    return this.isErrorName("required")
   }
 
   isMinLengthError(): boolean {
+    return this.isErrorName("minlength")
+  }
+
+  isMinError(): boolean {
+    return this.isErrorName("min")
+  }
+
+  isMaxError(): boolean {
+    return this.isErrorName("max")
+  }
+
+  isErrorName(errorName: string): boolean {
     if (!this.formControl.errors) {
       return false
     }
-    return !!this.formControl.errors["minlength"]
+    return !!this.formControl.errors[errorName]
   }
 }
