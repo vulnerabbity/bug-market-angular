cb62b74a721660217fd8380aa3ffab7cb04370d2
vulnerabbity <you@example.com>
2022-03-30 19:00:48 +0700

    add product abilities

--- src/app/features/products/products.module.ts
+++ src/app/features/products/products.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
 import { GraphqlModule } from "src/app/modules/graphql.module"
+import { ProductAbilities } from "./product.abilities"
 import { ProductsService } from "./products.service"
 
 @NgModule({
   imports: [CommonModule, GraphqlModule],
-  providers: [ProductsService],
+  providers: [ProductsService, ProductAbilities],
   exports: []
 })
 export class ProductsModule {}
