411ac77b58fd56734745c74e498aa859ef4f8e9f
vulnerabbity <you@example.com>
2022-03-30 14:12:04 +0700

    add image upload interfaces

--- src/app/features/products/products.interface.ts
+++ src/app/features/products/products.interface.ts
@@ -4,8 +4,23 @@ import { PaginatedFeature } from "../features.interfaces"
 export type ShortProduct = Pick<Product, "id" | "name" | "price" | "imagesUrls">
 export type PaginatedShortProducts = PaginatedFeature<ShortProduct>
 
-export type UploadProductResponse = {
+export interface UploadProductResponse {
   status: "success" | "error"
-  data?: string
+  productId?: string
   error?: "unknownError"
+}
+
+export interface UploadProductImageResponse {
+  status: "success" | "error"
+}
+
+export interface UploadManyProductImagesInput {
+  images: Blob[]
+  productId: string
+}
+
+export interface UploadSingleProductImageInput {
+  productId: string
+  imageIndex: number
+  image: Blob
 }
