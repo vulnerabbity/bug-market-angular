c4d97084640fa70842feb3ce04bdb05f2fa8f367
vulnerabbity <you@example.com>
2022-03-31 20:13:24 +0700

    initial

---
+++ src/app/pages/products/filters/filters-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "product-page-filters-sidebar",
+  templateUrl: "./filters-sidebar.component.html",
+  styleUrls: ["./filters-sidebar.component.scss"]
+})
+export class ProductPageFiltersSidebar {
+  showFiller = false
+}
