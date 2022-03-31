5a1388ab96d5cd5f510a8fa3a50c562112d35dc8
vulnerabbity <you@example.com>
2022-04-01 01:50:24 +0700

    add price range support

--- src/app/pages/products/filters/filters-sidebar.component.ts
+++ src/app/pages/products/filters/filters-sidebar.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from "@angular/core"
 import { initProductCategoryAutocompleteModel } from "src/app/common/components/autocomplete/category.component"
+import { RangeModel } from "src/app/common/components/range/range.component"
 import { ProductCategory } from "src/app/features/categories/categories.interface"
-import { SortingOrder as SortingOrderEnum } from "src/generated-gql-types"
+import { SortingOrder as SortingOrderEnum, ProductFilters } from "src/generated-gql-types"
 
 type SortingOrder = `${SortingOrderEnum}`
 export type SortingOptions = SortingOrder | "NO_SORTING"
@@ -9,6 +10,7 @@ export type SortingOptions = SortingOrder | "NO_SORTING"
 export interface ProductSidebarFilters {
   category: ProductCategory | null
   priceSorting: SortingOptions
+  priceRange?: ProductFilters["priceRange"]
 }
 
 @Component({
@@ -21,6 +23,8 @@ export class ProductPageFiltersSidebar implements OnInit {
 
   category = initProductCategoryAutocompleteModel()
 
+  rangeModel: RangeModel = { min: null, max: null }
+
   @Output()
   onApply = new EventEmitter<ProductSidebarFilters>()
 
@@ -31,10 +35,19 @@ export class ProductPageFiltersSidebar implements OnInit {
     this.onApply.next(filters)
   }
 
-  getCurrentFilters(): ProductSidebarFilters {
-    return {
+  private getCurrentFilters(): ProductSidebarFilters {
+    const filters: ProductSidebarFilters = {
       category: this.category.selectedCategory,
       priceSorting: this.priceSorting
     }
+
+    const { min: rangeMin, max: rangeMax } = this.rangeModel
+    const rangeSelected = rangeMax !== null
+
+    if (rangeSelected) {
+      filters.priceRange = { min: rangeMin ?? undefined, max: rangeMax }
+    }
+
+    return filters
   }
 }
