363e6a6a11329df22a8871e429f3673d7ddd70d1
vulnerabbity <you@example.com>
2022-03-31 20:10:13 +0700

    add sidenav

--- src/app/pages/products/products-page.module.ts
+++ src/app/pages/products/products-page.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core"
 import { AppCommonModule } from "src/app/common/modules/common.module"
 import { ProductsModule } from "src/app/features/products/products.module"
+import { ProductPageFiltersSidebar } from "./filters/filters-sidebar.component"
 import { ProductsNavbarComponent } from "./navbar/products-navbar.component"
 import { ProductsPageComponent } from "./products-page.component"
 
 @NgModule({
   imports: [AppCommonModule, ProductsModule],
-  declarations: [ProductsPageComponent, ProductsNavbarComponent]
+  declarations: [ProductsPageComponent, ProductsNavbarComponent, ProductPageFiltersSidebar]
 })
 export class ProductsPageModule {}
