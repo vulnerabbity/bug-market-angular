4478abc647a6b6541611662c86cecbbe49c9a8f3
vulnerabbity <you@example.com>
2022-04-01 15:35:09 +0700

    refactoring

---
+++ src/app/common/components/cards/cards.declaration.ts
@@ -0,0 +1,4 @@
+import { CommonTextCardComponent } from "./text-card/text-card.component"
+import { CommonTitleTextCardComponent } from "./title-text-card/title-text-card.component"
+
+export const commonCardsComponents = [CommonTitleTextCardComponent, CommonTextCardComponent]
