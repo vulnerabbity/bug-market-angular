2f92a77f6b53c1e9267cef6e3e63946a782431c1
vulnerabbity <you@example.com>
2022-04-03 21:47:03 +0700

    refactoring

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
-import { GraphqlModule } from "../modules/graphql.module"
+import { GraphqlModule } from "./graphql.module"
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
 import { AppNavbarComponent } from "./navbar/navbar.component"
