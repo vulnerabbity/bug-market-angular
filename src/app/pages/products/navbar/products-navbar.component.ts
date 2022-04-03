a9fd80ec96f15d1cd4133b236ccd9971a1b5a3fa
vulnerabbity <you@example.com>
2022-04-04 02:35:22 +0700

    refactoring

--- src/app/pages/products/navbar/products-navbar.component.ts
+++ src/app/pages/products/navbar/products-navbar.component.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core"
-import { AppPathsEnum } from "src/app/app/app-routing.module"
-import { ProductAbilities } from "src/app/features/products/product.abilities"
 import { UserStatusService } from "src/app/features/users/user-status.service"
 
 @Component({
@@ -9,28 +7,4 @@ import { UserStatusService } from "src/app/features/users/user-status.service"
   styleUrls: ["./products-navbar.component.scss"],
   providers: [UserStatusService]
 })
-export class ProductsNavbarComponent {
-  createProductPath = "/" + AppPathsEnum.CreateProduct
-
-  get createProductTooltipText(): string {
-    const cantCreateProduct = this.canCreateProduct()
-    if (cantCreateProduct) {
-      return "Add new product"
-    }
-    return "Login required"
-  }
-
-  constructor(private productAbilities: ProductAbilities) {}
-
-  getCreateProductPathIfAllowed(): string | null {
-    const isAllowed = this.canCreateProduct()
-    if (isAllowed) {
-      return this.createProductPath
-    }
-    return null
-  }
-
-  canCreateProduct(): boolean {
-    return this.productAbilities.canCreateProduct()
-  }
-}
+export class ProductsNavbarComponent {}
