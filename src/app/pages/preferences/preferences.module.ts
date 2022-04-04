eed6ddd70bccb18f9bde8328f51ae30f893eb621
vulnerabbity <you@example.com>
2022-04-05 01:21:41 +0700

    remove preferences

--- src/app/pages/preferences/preferences.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core"
-import { AppCommonComponentsModule } from "src/app/common/components/components.module"
-import { PreferencesPageComponent } from "./preferences-page.component"
-
-@NgModule({
-  imports: [AppCommonComponentsModule],
-  declarations: [PreferencesPageComponent]
-})
-export class PreferencesPageModule {}
