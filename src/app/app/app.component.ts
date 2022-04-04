72cd3c4b8e72983c2f6ea355aa0b87bfee790d98
vulnerabbity <you@example.com>
2022-04-05 01:13:16 +0700

    add dynamic themes support

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { ThemeLocalStorageService } from "../features/preferences/storage/theme-storage.service"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'bug-market-angular';
+  constructor(private theme: ThemeLocalStorageService) {}
+  title = "bug-market-angular"
+
+  getTheme() {
+    return this.theme.tryToGetRecord()
+  }
 }
