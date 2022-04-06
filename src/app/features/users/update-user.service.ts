632334f2ed0e1db69cfaf6b01951ad04435fd1bb
vulnerabbity <you@example.com>
2022-04-07 00:09:01 +0700

    add avatar update delete

--- src/app/features/users/update-user.service.ts
+++ src/app/features/users/update-user.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
-import { firstValueFrom, map, Observable } from "rxjs"
+import { catchError, firstValueFrom, map, Observable } from "rxjs"
+import { environment } from "src/environments/environment"
 import { UpdateUserGQL, UpdateUserInput } from "src/generated-gql-types"
 
 export type UpdateUserStatus = "success" | "error"
+export type UpdateAvatarStatus = "success" | "error"
+export type DeleteAvatarStatus = "success" | "error"
 
 @Injectable({
   providedIn: "root"
 })
 export class UpdateUserService {
-  constructor(private updateUserMutation: UpdateUserGQL) {}
+  constructor(private updateUserMutation: UpdateUserGQL, private http: HttpClient) {}
 
   async updateUserAsync(id: string, update: UpdateUserInput): Promise<UpdateUserStatus> {
     return firstValueFrom(this.updateUser$(id, update))
@@ -23,5 +27,42 @@ export class UpdateUserService {
         return "error"
       })
     )
+  }
+
+  async uploadAvatarAsync(userId: string, avatar: Blob): Promise<UpdateAvatarStatus> {
+    return firstValueFrom(this.uploadAvatar$(userId, avatar))
+  }
+
+  uploadAvatar$(userId: string, avatar: Blob): Observable<UpdateAvatarStatus> {
+    const uploadUrl = this.getAvatarManageEndpoint(userId)
+    const body = this.makeAvatarBody(avatar)
+    return this.http.post(uploadUrl, body).pipe(
+      catchError(() => "error"),
+      map(() => "success")
+    )
+  }
+
+  async deleteAvatarAsync(userId: string) {
+    return await firstValueFrom(this.deleteAvatar$(userId))
+  }
+
+  deleteAvatar$(userId: string): Observable<DeleteAvatarStatus> {
+    const url = this.getAvatarManageEndpoint(userId)
+    return this.http.delete(url).pipe(
+      catchError(() => "error"),
+      map(() => "success")
+    )
+  }
+
+  private getAvatarManageEndpoint(userId: string) {
+    return `${environment.backendUrl}/users/${userId}/avatar`
+  }
+
+  private makeAvatarBody(image: Blob): FormData {
+    // api accepts avatars with field name "avatar"
+    const formWithAvatar = new FormData()
+    formWithAvatar.append("avatar", image)
+
+    return formWithAvatar
   }
 }
