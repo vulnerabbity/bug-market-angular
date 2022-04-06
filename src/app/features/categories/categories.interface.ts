052f6a24ea61c77d9dda6683346a532cf46e8ac2
vulnerabbity <you@example.com>
2022-04-07 02:15:51 +0700

    update v1.2.3

--- src/app/features/categories/categories.interface.ts
+++ src/app/features/categories/categories.interface.ts
@@ -1,4 +1,46 @@
-export const ProductCategoryDatabaseNames = <const>["electronics", "cars", "other"]
+// version 1.2.3
+export const ProductCategoryDatabaseNames = <const>[
+  // machines
+  "cars",
+  "carsParts",
+  "carsEquipment",
+
+  "realEstate",
+
+  // electronics
+  "electronics",
+  "smartphones",
+  "headphones",
+  "computers",
+  "commutersEquipment",
+  "laptops",
+  "photoVideoEquipment",
+  "audioDevices",
+  "networkDevices",
+
+  // furniture
+  "furniture",
+  "houseAppliances",
+
+  // humans
+  "service",
+  "vacancy",
+  "resume",
+  "handmade",
+
+  "clothes",
+
+  //hobby
+  "hobby",
+  "sport",
+  "musicInstruments",
+
+  "childrenGoods",
+
+  "animals",
+
+  "other"
+]
 
 // Convert to union type
 export type ProductCategoryDatabaseName = typeof ProductCategoryDatabaseNames[number]
