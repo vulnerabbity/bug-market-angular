6761d6398282f2bcc4b42f5620eb4fe2999a9d59
vulnerabbity <you@example.com>
2022-04-06 18:35:00 +0700

    add router

--- src/app/common/services/services.module.ts
+++ src/app/common/services/services.module.ts
@@ -1,9 +1,9 @@
 import { NgModule, Provider } from "@angular/core"
 import { AppAuthorizationModule } from "src/app/features/authorization/authorization.module"
 import { AuthorizationService } from "src/app/features/authorization/authorization.service"
-import { FilesService } from "./files.service"
+import { AppRouterService } from "./router.service"
 
-const services: Provider[] = [FilesService, AuthorizationService]
+const services: Provider[] = [AuthorizationService, AppRouterService]
 
 @NgModule({
   imports: [AppAuthorizationModule],
