8eb39a5f7130938207966b4e36cdb2d1468969ac
vulnerabbity <you@example.com>
2022-04-09 18:24:34 +0700

    add redirect guard

--- src/app/pages/errors/error-router.service.ts
+++ src/app/pages/errors/error-router.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from "@angular/core"
 import { Router } from "@angular/router"
+import { HttpResponse } from "src/app/common/interfaces/responses.interface"
 import { ErrorsPaths } from "./errors-routing.module"
+
+export class RedirectionGuardError extends Error {
+  constructor() {
+    const message = "Error-Redirection-Guard"
+    super(message)
+  }
+}
 
 @Injectable({
   providedIn: "root"
 })
 export class ErrorsRouterService {
   constructor(private ngRouter: Router) {}
+
+  redirectIfError({ status }: HttpResponse) {
+    if (status === "success") {
+      return
+    }
+
+    if (status === "not-found") {
+      this.redirectToNotFound()
+    } else {
+      this.redirectToUnknown()
+    }
+
+    throw new RedirectionGuardError()
+  }
 
   redirectToUnknown() {
     this.redirect(ErrorsPaths.UnknownError)
