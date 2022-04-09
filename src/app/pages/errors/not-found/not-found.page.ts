67f86b9411a67a3002482a8e3808cf5934910f89
vulnerabbity <you@example.com>
2022-04-09 22:18:10 +0700

    add styles

--- src/app/pages/errors/not-found/not-found.page.ts
+++ src/app/pages/errors/not-found/not-found.page.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core"
 
 @Component({
-  template: "not found"
+  templateUrl: "./not-found.page.html",
+  styleUrls: ["./not-found.page.scss"]
 })
 export class ErrorPageNotFound {}
