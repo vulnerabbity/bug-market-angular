571e9bce01c41922177c825dfbfcfb23ce56e501
vulnerabbity <you@example.com>
2022-04-09 18:17:14 +0700

    add server unavailable

--- src/app/common/error-handling/error-handling.module.ts
+++ src/app/common/error-handling/error-handling.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core"
 import { RedirectGuardErrorHandlerProvider } from "src/app/pages/errors/redirect.error-handler"
+import { ServerUnavailableErrorHandlerProvider } from "src/app/pages/errors/server-unavailable/server-unavailable.handler"
 
 @NgModule({
-  providers: [RedirectGuardErrorHandlerProvider]
+  providers: [RedirectGuardErrorHandlerProvider, ServerUnavailableErrorHandlerProvider]
 })
 export class AppErrorHandlingModule {}
