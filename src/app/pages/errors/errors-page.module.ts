3c0aaa9f7877203ab78ede48fa899fcade0743f8
vulnerabbity <you@example.com>
2022-04-09 22:21:08 +0700

    add pages declaration

--- src/app/pages/errors/errors-page.module.ts
+++ src/app/pages/errors/errors-page.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core"
+import { AppCommonModule } from "src/app/common/common.module"
 import { ErrorsRouterService } from "./error-router.service"
 import { ErrorsRoutingModule } from "./errors-routing.module"
+import { ErrorPageNotFound } from "./not-found/not-found.page"
+import { ErrorPageServerUnavailable } from "./server-unavailable/server-unavailable.page"
+import { ErrorPageUnknown } from "./unknown-error/unknown-error.page"
 
 @NgModule({
-  imports: [ErrorsRoutingModule],
-  providers: [ErrorsRouterService]
+  imports: [ErrorsRoutingModule, AppCommonModule],
+  providers: [ErrorsRouterService],
+  declarations: [ErrorPageNotFound, ErrorPageServerUnavailable, ErrorPageUnknown]
 })
 export class ErrorsPageModule {}
