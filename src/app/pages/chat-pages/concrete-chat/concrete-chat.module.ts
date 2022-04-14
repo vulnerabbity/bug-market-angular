35e028f851cfae4ce3bf8074cd1c197b51632844
vulnerabbity <you@example.com>
2022-04-14 22:56:24 +0700

    refactoring

--- src/app/pages/chat-pages/concrete-chat/concrete-chat.module.ts
+++ src/app/pages/chat-pages/concrete-chat/concrete-chat.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from "@angular/core"
 import { AppCommonModule } from "src/app/common/common.module"
 import { ChatFeatureModule } from "src/app/features/chat/chat.feature.module"
 import { ConcreteChatPage } from "./concrete-chat.page"
-import { ConcreteChatHeader } from "./header/concrete-chat-navbar.component"
 import { MessagesBoxComponent } from "./messages-box/messages-box.component"
+import { TopNavbarComponent } from "./top-navbar/top-navbar.component"
 
 @NgModule({
   imports: [AppCommonModule, ChatFeatureModule],
-  declarations: [ConcreteChatPage, ConcreteChatHeader, MessagesBoxComponent]
+  declarations: [ConcreteChatPage, TopNavbarComponent, MessagesBoxComponent]
 })
 export class ConcreteChatModule {}
