66059b3c81ba78ea79d9431c2fcb3f9d279a32bb
vulnerabbity <you@example.com>
2022-04-14 23:56:50 +0700

    add initial bottom navbar

---
+++ src/app/pages/chat-pages/concrete-chat/bottom-navbar/bottom-navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "concrete-chat__bottom-navbar",
+  templateUrl: "./bottom-navbar.component.html",
+  styleUrls: ["./bottom-navbar.component.scss"]
+})
+export class BottomNavbarComponent {}
