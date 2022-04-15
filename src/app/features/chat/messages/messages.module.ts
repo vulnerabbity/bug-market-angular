7288136fa497a3e734cd52be21f5f9ba17f36a92
vulnerabbity <you@example.com>
2022-04-15 20:48:45 +0700

    add message type service

--- src/app/features/chat/messages/messages.module.ts
+++ src/app/features/chat/messages/messages.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core"
+import { MessageTypeService } from "./message-type.service"
 import { MessagesLoader } from "./messages-loader.service"
 import { MessagesSenderService } from "./messages-sender.service"
 
 @NgModule({
-  providers: [MessagesLoader, MessagesSenderService]
+  providers: [MessagesLoader, MessagesSenderService, MessageTypeService]
 })
 export class ChatMessagesModule {}
