fd14754ba6dca48b84d1be7da13e9716af316561
vulnerabbity <you@example.com>
2022-04-15 21:00:56 +0700

    add logout with state

--- src/app/features/users/current-user.state.ts
+++ src/app/features/users/current-user.state.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core"
 import { BehaviorSubject, from, Observable } from "rxjs"
 import { ReactiveState } from "src/app/common/interfaces/state.interface"
+import { AppRouterService } from "src/app/common/services/router.service"
+import { AuthenticationService } from "../authentication/authentication.service"
 import { LocalUserService } from "./local-user.service"
 import { UsersLoaderService } from "./users-loader.service"
 import { User } from "./users.interface"
@@ -13,12 +15,23 @@ export class CurrentUserState implements ReactiveState<User | null> {
 
   item$: Observable<User | null> = from(this.subject$)
 
-  constructor(private usersLoader: UsersLoaderService, private localUserService: LocalUserService) {
+  constructor(
+    private usersLoader: UsersLoaderService,
+    private localUserService: LocalUserService,
+    private authenticationService: AuthenticationService,
+    private appRouter: AppRouterService
+  ) {
     this.initState()
   }
 
-  setItem(input: User) {
+  setItem(input: User | null) {
     this.subject$.next(input)
+  }
+
+  async logout() {
+    this.appRouter.redirectHome()
+    this.authenticationService.logout()
+    this.setItem(null)
   }
 
   async fetchState() {
