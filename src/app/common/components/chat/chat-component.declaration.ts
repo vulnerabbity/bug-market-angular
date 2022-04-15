833dcd7d89745eeac534b88d168c24b5391eda39
vulnerabbity <you@example.com>
2022-04-15 18:26:26 +0700

    add message component

--- src/app/common/components/chat/chat-component.declaration.ts
+++ src/app/common/components/chat/chat-component.declaration.ts
@@ -1,4 +1,9 @@
 import { CommonChatItemComponent } from "./chat-item/chat-item.component"
 import { CommonChatContainerComponent } from "./container/chats-container.component"
+import { CommonChatTextMessageComponent } from "./message/chat-text-message.component"
 
-export const commonChatComponents = [CommonChatContainerComponent, CommonChatItemComponent]
+export const commonChatComponents = [
+  CommonChatContainerComponent,
+  CommonChatItemComponent,
+  CommonChatTextMessageComponent
+]
