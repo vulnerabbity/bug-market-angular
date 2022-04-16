40cef4e53f0331a3aaa5b3e43ee5ed54ac686aef
vulnerabbity <you@example.com>
2022-04-16 20:32:26 +0700

    add websocket url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,12 @@
 import { AppEnvironment } from "./environment.interface"
 
 const backendUrl = "https://bug-market-nest.herokuapp.com"
+const websocketsUrl = "https://bug-market-nest.herokuapp.com:3003"
 
 export const environment: AppEnvironment = {
   production: false,
   publicFilesUrl: "https://bug-market-nest.herokuapp.com/public-files",
   backendUrl,
-  hostsForTokenUsage: [backendUrl]
+  hostsForTokenUsage: [backendUrl],
+  websocketsUrl
 }
