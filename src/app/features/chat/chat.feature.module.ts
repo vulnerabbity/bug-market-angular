fb78fc97c084e984eefcf4d01879247cab4dc928
vulnerabbity <you@example.com>
2022-04-16 20:12:06 +0700

    add chat notifications module

--- src/app/features/chat/chat.feature.module.ts
+++ src/app/features/chat/chat.feature.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core"
 import { ChatsModule } from "./chats/chats.module"
 import { ChatMessagesModule } from "./messages/messages.module"
+import { ChatsNotificationsModule } from "./notifications/notifications.module"
 
 @NgModule({
-  imports: [ChatMessagesModule, ChatsModule]
+  imports: [ChatMessagesModule, ChatsModule, ChatsNotificationsModule]
 })
 export class ChatFeatureModule {}
---
+++ src/app/features/chat/notifications/notifications.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core"
+import { ChatNotificationsService } from "./chat-notifications.service"
+
+@NgModule({
+  providers: [ChatNotificationsService]
+})
+export class ChatsNotificationsModule {}
