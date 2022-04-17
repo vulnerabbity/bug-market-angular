5b6d28f2804390e4be332c7a203f61928e57fe46
vulnerabbity <you@example.com>
2022-04-17 17:48:01 +0700

    add chat opened event emission

--- src/app/pages/chat-pages/concrete-chat/concrete-chat.page.ts
+++ src/app/pages/chat-pages/concrete-chat/concrete-chat.page.ts
@@ -1,29 +1,45 @@
 import { Component, OnInit } from "@angular/core"
 import { ActivatedRoute } from "@angular/router"
-import { firstValueFrom, map } from "rxjs"
+import { filter, firstValueFrom, map } from "rxjs"
+import { ExtendedChat } from "src/app/features/chat/chats/chat.interface"
 import { CurrentChatState } from "src/app/features/chat/chats/current-chat.state"
+import { ChatEvents } from "src/app/features/chat/notifications/chat.events"
 
 @Component({
   templateUrl: "./concrete-chat.page.html",
   styleUrls: ["./concrete-chat.page.scss"]
 })
 export class ConcreteChatPage implements OnInit {
-  currentChatId!: string
-
-  constructor(private chatState: CurrentChatState, private currentRoute: ActivatedRoute) {}
+  constructor(
+    private chatState: CurrentChatState,
+    private currentRoute: ActivatedRoute,
+    private chatEvents: ChatEvents
+  ) {}
 
   ngOnInit() {
     this.initChat()
   }
 
   private async initChat() {
-    await this.loadChatId()
-    this.chatState.init(this.currentChatId)
+    const chatId = await this.getChatId()
+    this.chatState.init(chatId)
+
+    const fetchedChat$ = this.chatState.chat$.pipe(
+      filter(chat => chat !== null),
+      map(chat => chat!)
+    )
+    const chat = await firstValueFrom(fetchedChat$)
+
+    this.emitChatOpened(chat)
   }
 
-  private async loadChatId(): Promise<void> {
+  private async getChatId(): Promise<string> {
     const chatId$ = this.currentRoute.params.pipe(map(params => params["id"]))
     const chatId = await firstValueFrom(chatId$)
-    this.currentChatId = chatId
+    return chatId
+  }
+
+  private async emitChatOpened(chat: ExtendedChat) {
+    this.chatEvents.chatOpened$.next(chat)
   }
 }
