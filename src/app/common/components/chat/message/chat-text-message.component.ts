d94dc7cb85e9a9878fa23a9df9a1f0e92376198d
vulnerabbity <you@example.com>
2022-04-17 19:17:29 +0700

    bugfix: invalid message viewed cheker

--- src/app/common/components/chat/message/chat-text-message.component.ts
+++ src/app/common/components/chat/message/chat-text-message.component.ts
@@ -1,7 +1,4 @@
-import { Component, Input, OnDestroy } from "@angular/core"
-import { MaterialColor } from "src/app/common/interfaces/material.interface"
-import { CurrentUserState } from "src/app/features/users/current-user.state"
-import { User } from "src/app/features/users/users.interface"
+import { Component, Input } from "@angular/core"
 import { ChatMessage } from "src/generated-gql-types"
 
 @Component({
@@ -9,41 +6,29 @@ import { ChatMessage } from "src/generated-gql-types"
   templateUrl: "./chat-text-message.component.html",
   styleUrls: ["./chat-text-message.component.scss"]
 })
-export class CommonChatTextMessageComponent implements OnDestroy {
-  currentUser: User | null = null
-
+export class CommonChatTextMessageComponent {
   @Input()
   message!: ChatMessage
 
   @Input()
   color: "accent" | "primary" = "primary"
 
-  private currentUserSub = this.subscribeToUser()
-
-  constructor(private currentUserState: CurrentUserState) {}
-
-  ngOnDestroy(): void {
-    this.currentUserSub.unsubscribe()
-  }
+  constructor() {}
 
   isViewedByOthers() {
-    const currentId = this.currentUser?.id ?? ""
-    const otherViewers = this.message.viewedBy.filter(viewerId => viewerId !== currentId)
+    const senderId = this.message.userId
+    const otherViewers = this.message.viewedBy.filter(viewerId => viewerId !== senderId)
 
     const hasViewers = otherViewers.length > 0
     return hasViewers
   }
 
-  isViewedByCurrentUser(): boolean {
-    const currentUser = this.currentUser
-    if (!currentUser) {
+  isViewedBySender(): boolean {
+    const senderId = this.message.userId
+    if (!senderId) {
       return false
     }
 
-    return this.message.viewedBy.includes(currentUser.id)
-  }
-
-  private subscribeToUser() {
-    return this.currentUserState.item$.subscribe(currentUser => (this.currentUser = currentUser))
+    return this.message.viewedBy.includes(senderId)
   }
 }
