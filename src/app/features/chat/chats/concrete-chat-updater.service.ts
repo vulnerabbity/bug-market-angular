208ca52ac10396d390cd7e5c9a813ccda51f0c94
vulnerabbity <you@example.com>
2022-04-17 19:15:22 +0700

    refactoring

--- src/app/features/chat/chats/concrete-chat-updater.service.ts
+++ src/app/features/chat/chats/concrete-chat-updater.service.ts
@@ -1,46 +1,14 @@
 import { Injectable } from "@angular/core"
-import { ChatMessage } from "src/generated-gql-types"
 import { WebsocketsService } from "../../websockets/ws.service"
-import { ChatEvents } from "../notifications/chat.events"
-import { ExtendedChat } from "./chat.interface"
-import { CurrentChatState } from "./current-chat.state"
 
 @Injectable({ providedIn: "root" })
 export class ConcreteChatsUpdaterService {
-  currentChat: ExtendedChat | null = null
-
   private socket = this.ws.getSocket()
 
-  constructor(
-    private ws: WebsocketsService,
-    private chatEvents: ChatEvents,
-    private currentChatState: CurrentChatState
-  ) {
-    this.currentChatState.chat$.subscribe(chat => (this.currentChat = chat))
-
-    this.viewMessagesOnOpen()
-    this.viewMessagesOnReceive()
-  }
+  constructor(private ws: WebsocketsService) {}
 
   viewMessages(chatId: string) {
     console.log("viewed")
     this.socket.emit("markChatViewed", { chatId })
-  }
-
-  private viewMessagesOnOpen() {
-    this.chatEvents.chatOpened$.subscribe(chat => this.viewMessages(chat.id))
-  }
-
-  private viewMessagesOnReceive() {
-    this.chatEvents.messageReceived$.subscribe(newMessage => {
-      const isMessageForCurrentChat = this.isMessageForCurrentChat(newMessage)
-      if (isMessageForCurrentChat) {
-        this.viewMessages(this.currentChat!.id)
-      }
-    })
-  }
-
-  private isMessageForCurrentChat(newMessage: ChatMessage) {
-    return this.currentChat?.id === newMessage.chatId
   }
 }
