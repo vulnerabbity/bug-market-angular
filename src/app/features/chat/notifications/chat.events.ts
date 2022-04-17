04d0b90f097c44818b1bbec031ebc8e7be6782e4
vulnerabbity <you@example.com>
2022-04-17 17:47:18 +0700

    add chat opened event

--- src/app/features/chat/notifications/chat.events.ts
+++ src/app/features/chat/notifications/chat.events.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@angular/core"
 import { Subject } from "rxjs"
 import { ChatMessage } from "src/generated-gql-types"
+import { ExtendedChat } from "../chats/chat.interface"
 
 @Injectable({ providedIn: "root" })
 export class ChatEvents {
   messageReceived$ = new Subject<ChatMessage>()
+  chatOpened$ = new Subject<ExtendedChat>()
 }
