32272b11e20e3103e7c8787d11ebb0afaa5647b6
vulnerabbity <you@example.com>
2022-04-18 09:13:17 +0700

    add message update feature

--- src/app/features/chat/messages/messages.events-handler.ts
+++ src/app/features/chat/messages/messages.events-handler.ts
@@ -12,12 +12,41 @@ export class MessagesEventHandler {
 
   private startHandling() {
     this.handleReceiveMessage()
+    this.handleUpdateMessage()
   }
 
   private handleReceiveMessage() {
     this.chatEvents.messageReceived$.subscribe(newMessage => {
       this.addMessageToState(newMessage)
     })
+  }
+
+  private handleUpdateMessage() {
+    this.chatEvents.messageUpdated$.subscribe(updatedMessage => {
+      this.updateMessageIfInCurrentChat(updatedMessage)
+    })
+  }
+
+  private updateMessageIfInCurrentChat(updatedMessage: ChatMessage) {
+    const messages = this.messagesState.messages
+    const messageIndex = this.findMessageIndexOrNull(messages, updatedMessage.id)
+
+    if (messageIndex !== null) {
+      return this.replaceMessageInStateAt(updatedMessage, messageIndex)
+    }
+  }
+
+  private replaceMessageInStateAt(message: ChatMessage, atIndex: number) {
+    const messages = this.messagesState.messages
+    const lastMessagesIndex = messages.length - 1
+
+    const isOutOfRange = atIndex < 0 || atIndex > lastMessagesIndex
+    if (isOutOfRange) {
+      return
+    }
+
+    messages[atIndex] = message
+    this.messagesState.messages$.next(messages)
   }
 
   private addMessageToState(newMessage: ChatMessage) {
@@ -29,5 +58,16 @@ export class MessagesEventHandler {
     messages.unshift(newMessage)
 
     this.messagesState.messages$.next(messages)
+  }
+
+  private findMessageIndexOrNull(messages: ChatMessage[], idToSearch: string): number | null {
+    const messageIndex = messages.findIndex(message => message.id === idToSearch)
+    const notFoundIndex = -1
+
+    if (messageIndex === notFoundIndex) {
+      return null
+    }
+
+    return messageIndex
   }
 }
--- src/app/features/chat/messages/messages.module.ts
+++ src/app/features/chat/messages/messages.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from "@angular/core"
 import { MessageTypeService } from "./message-type.service"
 import { MessagesLoader } from "./messages-loader.service"
 import { MessagesSenderService } from "./messages-sender.service"
+import { MessagesEventHandler } from "./messages.events-handler"
 import { CurrentChatMessagesState } from "./messages.state"
 
 @NgModule({
-  providers: [MessagesLoader, MessagesSenderService, MessageTypeService, CurrentChatMessagesState]
+  providers: [MessagesLoader, MessagesSenderService, MessageTypeService]
 })
-export class ChatMessagesModule {}
+export class ChatMessagesModule {
+  // instantiate on module loaded
+  constructor(
+    private messagesEventsHandler: MessagesEventHandler,
+    private messagesState: CurrentChatMessagesState
+  ) {}
+}
