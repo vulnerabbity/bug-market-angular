fc179798e52ce7613ef1e24f71719156192d614c
vulnerabbity <you@example.com>
2022-04-19 10:40:38 +0700

    add total not viewed messages listening

--- src/app/features/chat/notifications/chat-notifications.service.ts
+++ src/app/features/chat/notifications/chat-notifications.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from "@angular/core"
 import { Socket } from "socket.io-client"
 import { WebsocketsService } from "../../websockets/ws.service"
+import { ChatMessagesNumberLoader } from "../messages/messages-number-loader"
 import { ChatEvents } from "./chat.events"
 
 @Injectable({
   providedIn: "root"
 })
 export class ChatNotificationsService {
-  constructor(private chatEvents: ChatEvents, private ws: WebsocketsService) {}
+  constructor(
+    private chatEvents: ChatEvents,
+    private ws: WebsocketsService,
+    private messagesNumbersLoader: ChatMessagesNumberLoader
+  ) {}
 
   private socket!: Socket
 
   private messageReceivedEvent = "messageReceived"
   private messageUpdatedEvent = "messageUpdated"
+  private totalNotViewedMessagesChangedEvent = "totalNotViewedMessagesChanged"
 
   async listen() {
     this.socket = this.ws.getSocket()
@@ -21,11 +27,13 @@ export class ChatNotificationsService {
 
     this.listenToMessageReceived()
     this.listenToMessageUpdated()
+    this.listenToTotalNotViewedMessages()
   }
 
   stopListening() {
     this.socket.off(this.messageReceivedEvent)
     this.socket.off(this.messageUpdatedEvent)
+    this.socket.off(this.totalNotViewedMessagesChangedEvent)
   }
 
   private listenToMessageReceived() {
@@ -38,5 +46,17 @@ export class ChatNotificationsService {
     this.socket.on(this.messageUpdatedEvent, message => {
       this.chatEvents.messageUpdated$.next(message)
     })
+  }
+
+  private listenToTotalNotViewedMessages() {
+    this.initTotalNotViewedMessages()
+    this.socket.on(this.totalNotViewedMessagesChangedEvent, (number: number) => {
+      this.chatEvents.notViewedMessagesTotalChanged$.next(number)
+    })
+  }
+
+  private async initTotalNotViewedMessages() {
+    const number = await this.messagesNumbersLoader.getTotal()
+    this.chatEvents.notViewedMessagesTotalChanged$.next(number)
   }
 }
