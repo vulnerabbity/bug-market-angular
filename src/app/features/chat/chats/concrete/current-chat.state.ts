5288c7eb0f726dd8998db2fb3196d15c2c4a2949
vulnerabbity <you@example.com>
2022-04-21 15:31:57 +0700

    remove router from state

--- src/app/features/chat/chats/concrete/current-chat.state.ts
+++ src/app/features/chat/chats/concrete/current-chat.state.ts
@@ -1,7 +1,5 @@
 import { Injectable } from "@angular/core"
 import { BehaviorSubject, Subject } from "rxjs"
-import { AppRouterService } from "src/app/common/services/router.service"
-import { ChatMessage } from "src/generated-gql-types"
 import { ExtendedChat } from "../many/chat.interface"
 import { ConcreteChatDeleterService } from "./concrete-chat-deleter.service"
 import { ConcreteExtendedChatLoader } from "./concrete-extended-chat-loader.service"
@@ -14,20 +12,18 @@ export class CurrentChatState {
 
   quit$ = new Subject<void>()
 
-  private chatSub = this.subscribeToChat()
-
   constructor(
-    private appRouter: AppRouterService,
     private chatLoader: ConcreteExtendedChatLoader,
     private chatDeleter: ConcreteChatDeleterService
-  ) {}
+  ) {
+    this.subscribeToChat()
+  }
 
   async init(chatId: string) {
     await this.initChat(chatId)
   }
 
   quit() {
-    this.appRouter.redirectToChats()
     this.chat$.next(null)
     this.quit$.next()
   }
