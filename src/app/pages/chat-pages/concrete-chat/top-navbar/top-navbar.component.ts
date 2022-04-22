b66fabdceddfb1e80a3c2e68d7de9950a6c2dc35
vulnerabbity <you@example.com>
2022-04-22 16:46:30 +0700

    add link to user in chat

--- src/app/pages/chat-pages/concrete-chat/top-navbar/top-navbar.component.ts
+++ src/app/pages/chat-pages/concrete-chat/top-navbar/top-navbar.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from "@angular/core"
+import { AppLinksService } from "src/app/common/services/links.service"
 import { AppRouterService } from "src/app/common/services/router.service"
 import { CurrentChatState } from "src/app/features/chat/chats/concrete/current-chat.state"
 import { ExtendedChat } from "src/app/features/chat/chats/many/chat.interface"
+import { CurrentUserState } from "src/app/features/users/current-user.state"
 import { assetsPaths } from "src/assets/assets.paths"
 
 @Component({
@@ -14,7 +16,16 @@ export class TopNavbarComponent implements OnDestroy {
 
   chatSubscription = this.chatState.chat$.subscribe(chat => (this.chat = chat))
 
-  constructor(private chatState: CurrentChatState, private appRouter: AppRouterService) {}
+  constructor(
+    private chatState: CurrentChatState,
+    private appRouter: AppRouterService,
+    private appLinks: AppLinksService,
+    private currentUserState: CurrentUserState
+  ) {}
+
+  ngOnDestroy(): void {
+    this.chatSubscription.unsubscribe()
+  }
 
   getChatImage() {
     const chatImage = this.chat?.chatImage ?? assetsPaths.NoAvatar
@@ -22,8 +33,21 @@ export class TopNavbarComponent implements OnDestroy {
     return chatImage
   }
 
-  ngOnDestroy(): void {
-    this.chatSubscription.unsubscribe()
+  getLinkToOtherUser(): string {
+    const currentUser = this.currentUserState.item
+    const peersIds = this.chat?.peersIds
+
+    const cantGetLink = !currentUser || !peersIds
+    if (cantGetLink) {
+      return ""
+    }
+
+    const otherUserId = peersIds.find(peerId => peerId !== currentUser.id)
+    if (!otherUserId) {
+      return ""
+    }
+
+    return this.appLinks.getLinkToUser(otherUserId)
   }
 
   onBackClick() {
