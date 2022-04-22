a9fca301d15dd6d047d7faf230c3b51e4460c70a
vulnerabbity <you@example.com>
2022-04-22 19:20:12 +0700

    refactoring

--- src/app/common/components/products-container/products-container.component.ts
+++ src/app/common/components/products-container/products-container.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input, OnInit } from "@angular/core"
 import { ShortProduct } from "src/app/features/products/products.interface"
+import { AppLinksService } from "../../services/links.service"
 
 @Component({
   selector: "common-products-container",
   templateUrl: "./products-container.component.html",
   styleUrls: ["./products-container.component.scss"]
 })
-export class CommonProductsContainer implements OnInit {
+export class CommonProductsContainer {
   @Input()
   products: ShortProduct[] = []
 
-  ngOnInit(): void {}
+  constructor(private linksService: AppLinksService) {}
+
+  getProductLink(product: ShortProduct) {
+    return this.linksService.getLinkToProduct(product.id)
+  }
 }
