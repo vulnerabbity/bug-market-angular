9487d710846fd011d14f047bf207d0ac294ef2ef
vulnerabbity <you@example.com>
2022-04-22 15:16:17 +0700

    add github image

--- src/assets/assets.paths.ts
+++ src/assets/assets.paths.ts
@@ -6,5 +6,6 @@ export const assetsPaths = {
   Icons: iconsPath,
   NoAvatar: `${picturesPath}/no-avatar.png`,
   NoImage: `${picturesPath}/no-image.webp`,
+  Github: `${picturesPath}/github.png`,
   Bug1: `${iconsPath}/bug1.svg`
 }
