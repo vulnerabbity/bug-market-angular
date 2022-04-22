c286575c70562cf94c9d493410fc61c3d5f3bddc
vulnerabbity <you@example.com>
2022-04-22 20:38:43 +0700

    add responsivity

--- src/app/common/components/buttons/links/home.component.ts
+++ src/app/common/components/buttons/links/home.component.ts
@@ -3,7 +3,26 @@ import { Component } from "@angular/core"
 @Component({
   selector: "common-home-link",
   template: `
-    <common-link text="Bug market" label="home" link="/" matIconName="home"></common-link>
+    <common-link class="small-home" label="home" link="/" matIconName="bug_report"></common-link>
+    <common-link
+      class="large-home"
+      text="Bug market"
+      label="home"
+      link="/"
+      matIconName="bug_report"
+    ></common-link>
+    <style>
+      @media (max-width: 30rem) {
+        .large-home {
+          display: none;
+        }
+      }
+      @media (min-width: 30rem) {
+        .small-home {
+          display: none;
+        }
+      }
+    </style>
   `
 })
 export class CommonHomeLinkComponent {}
