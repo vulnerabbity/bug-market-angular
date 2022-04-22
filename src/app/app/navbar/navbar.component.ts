fdbf12fb9532f955fe4f640950d21c282e6f9a93
vulnerabbity <you@example.com>
2022-04-22 20:38:11 +0700

    make navbar more responsive

--- src/app/app/navbar/navbar.component.ts
+++ src/app/app/navbar/navbar.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core"
+import { AppLinksService } from "src/app/common/services/links.service"
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
-export class AppNavbarComponent {}
+export class AppNavbarComponent {
+  constructor(private linksService: AppLinksService) {}
+
+  productsLink = this.linksService.getLinkToProducts()
+}
