9e8e24f69e7f57c99ec14a3e255acce3be723bbd
vulnerabbity <you@example.com>
2022-07-08 06:09:48 +0700

    removed title lenght limit

--- src/app/common/components/core/page-header/page-header-with-title.component.ts
+++ src/app/common/components/core/page-header/page-header-with-title.component.ts
@@ -4,13 +4,13 @@ import { Component, Input } from "@angular/core"
   selector: "common-titled-page-header",
   styleUrls: ["./page-header-with-title.component.scss"],
   template: `
-    <common-page-header>
-      <!-- Limit length without accessibility affect -->
-      <h1 class="visually-hidden">{{ title }}</h1>
-      <p class="title" role="none">{{ getProcessedTitle() }}</p>
+    <common-themed-div color="primary">
+      <div class="component-root">
+        <h1 class="title" role="none">{{ title }}</h1>
 
-      <ng-content class="input-content"></ng-content>
-    </common-page-header>
+        <ng-content class="input-content"></ng-content>
+      </div>
+    </common-themed-div>
   `
 })
 export class CommonPageHeaderWithTitleComponent {
@@ -19,28 +19,4 @@ export class CommonPageHeaderWithTitleComponent {
 
   @Input()
   titleLimit: number | null = null
-
-  private get hasTitleLimit(): boolean {
-    return this.titleLimit !== null
-  }
-
-  getProcessedTitle() {
-    const needCutTitle = this.hasTitleLimit
-
-    if (needCutTitle) {
-      const titleLimit = this.titleLimit as number
-      return this.getShortTitle(titleLimit)
-    }
-
-    return this.title
-  }
-
-  private getShortTitle(length = 10) {
-    const needCut = this.title.length > length
-
-    if (needCut === false) {
-      return this.title
-    }
-    return this.title.slice(0, length) + "..."
-  }
 }
